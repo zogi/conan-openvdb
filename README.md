79fcc12f7973eb43d10a37e1a2d2990b92035614
Zoltan Gilian <zoltan.gilian@gmail.com>
2017-05-24 21:30:09 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# What's this?
+
+This is a conan package for DreamWorks' [OpenVDB](http://www.openvdb.org/)
+library.
+
+# How to use it?
+
+Install [python 2.7+](https://www.python.org/download/releases/2.7/) and
+[conan](http://conanio.readthedocs.io/en/latest/installation.html).
+
+Clone this repository. From the directory where you cloned the repo run:
+
+```
+$ conan export zogi/testing
+```
+
+Write a
+[conanfile](http://conanio.readthedocs.io/en/latest/reference/conanfile_txt.html)
+including `OpenVDB/4.0.2@zogi/testing` in the `[requires]` section.
+If you use CMake, add `${CONAN_LIBS}` to the list of libs to link against.
+From your build directory run
+
+```bash
+$ conan install --build=missing path_to_your_conanfile
+```
+
+Now just run cmake as usual to build.
+
+### TBB and Boost
+
+TBB and Boost are dependencies of OpenVDB, but being a bit bulky, are not
+included in the conanfile as dependencies. This has the benefit of e.g. being
+able to link against Maya's TBB if OpenVDB is used in a Maya plugin without
+mentioning Maya in this conan package.
+This also has the consequence that TBB and Boost have to be installed to
+somewhere CMake can find in order to build and use this package.
+
+On Windows the `BOOST_ROOT` and `TBB_ROOT` envvars should point to the Boost
+and TBB root directories respectively so as to help CMake find them.
+
+# Testing
+
+This conan package has been tested on Windows 10 and MSVC 15.0. Testing on
+CentOS using gcc 4.8 is planned in the near future.
+
+If you use this on any other platform/configuration mentioned above, please
+inform me of any problems you encounter.
---
+++ conanfile.py
@@ -0,0 +1,95 @@
+import os
+import shutil
+from conans import ConanFile, CMake, tools
+
+
+class OpenVDBConan(ConanFile):
+    name = "OpenVDB"
+    version = "4.0.2"
+    license = "BSD"
+    description = "OpenVDB is an open source C++ library comprising a novel hierarchical data structure and a large suite of tools for the efficient storage and manipulation of sparse volumetric data discretized on three-dimensional grids."
+    url = "https://github.com/zogi/conan-openvdb"
+    requires = ( "glew/2.0.0@coding3d/stable"
+               , "blosc/1.11.2@zogi/stable"
+               , "zlib/1.2.8@lasote/stable"
+               , "IlmBase/2.2.0@Mikayex/stable"
+               , "OpenEXR/2.2.0@Mikayex/stable"
+               )
+    generators = "cmake"
+    settings = "os", "arch", "compiler", "build_type"
+    options = { "shared": [True, False]
+              , "fPIC": [True, False]
+              }
+    default_options = "shared=False", "fPIC=False"
+    exports = ["CMakeLists.txt"]
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            self.options.remove("fPIC")
+
+    def config(self):
+        if self.options.shared and "fPIC" in self.options.fields:
+            self.options.fPIC = True
+
+    def source(self):
+        self.run("git clone https://github.com/dreamworksanimation/openvdb" +
+                 " -c advice.detachedHead=false -b v%s src" % self.version)
+        tools.replace_in_file("src/CMakeLists.txt", "PROJECT ( OpenVDB )",
+                              "PROJECT ( OpenVDB )\n" +
+                              "include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)\n" +
+                              "conan_basic_setup()")
+        shutil.copy("CMakeLists.txt", "src/openvdb/CMakeLists.txt")
+
+    def build(self):
+
+        os.environ.update(
+            { "BLOSC_ROOT": self.deps_cpp_info["blosc"].rootpath
+            , "ILMBASE_ROOT": self.deps_cpp_info["IlmBase"].rootpath
+            , "OPENEXR_ROOT": self.deps_cpp_info["OpenEXR"].rootpath
+            , "GLEW_ROOT": self.deps_cpp_info["glew"].rootpath
+            })
+
+        cmake = CMake(self)
+
+        cmake.definitions.update(
+            { "BUILD_SHARED": self.options.shared
+            , "BUILD_TOOLS": False
+            , "OPENVDB_BUILD_CORE": True
+            , "OPENVDB_BUILD_UNITTESTS": False
+            , "OPENVDB_BUILD_PYTHON_MODULE": False
+            , "OPENVDB_ENABLE_3_ABI_COMPATIBLE": True
+            , "CMAKE_INSTALL_PREFIX": self.package_folder
+            })
+
+        tbb_root = os.environ.get("TBB_ROOT")
+        if tbb_root:
+            cmake.definitions.update(
+                { "TBB_LIBRARY_PATH": tbb_root + "/lib"
+                })
+
+        if "fPIC" in self.options.fields:
+            cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = self.options.fPIC
+
+        cmake.configure(source_dir="src")
+        cmake.build(target="install")
+
+    def package(self):
+        pass
+
+    def package_info(self):
+        if self.settings.os == "Windows" and not self.options.shared:
+            self.cpp_info.libs = ["libopenvdb"]
+        else:
+            self.cpp_info.libs = ["openvdb"]
+
+        self.cpp_info.defines = [ "OPENVDB_3_ABI_COMPATIBLE"
+                                , "OPENVDB_USE_BLOSC"
+                                ]
+
+        if self.options.shared:
+            self.cpp_info.defines.append("OPENVDB_DLL")
+        else:
+            self.cpp_info.defines.append("OPENVDB_STATICLIB")
+
+        if not self.options["OpenEXR"].shared:
+            self.cpp_info.defines.append("OPENVDB_OPENEXR_STATICLIB")
