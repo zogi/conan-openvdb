8a66cc25e2e30e803939d1786a922db28a09bc14
Zoltan Gilian <zoltan.gilian@gmail.com>
2017-08-19 11:05:23 +0200

    use configure instead of config

--- conanfile.py
+++ conanfile.py
@@ -29,7 +29,7 @@ class OpenVDBConan(ConanFile):
         if self.settings.os == "Windows":
             self.options.remove("fPIC")
 
-    def config(self):
+    def configure(self):
         if self.options.shared and "fPIC" in self.options.fields:
             self.options.fPIC = True
 
